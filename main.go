6d32b12a8aabfa4c26a5ebc94f7307478e9aae3a
gurunelee <dlckdgk4858@gmail.com>
2020-10-23 14:54:16 +0900

    handle 'getData' error

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -46,6 +47,10 @@ func main() {
 	router.HandleFunc("/signup/face", func(w http.ResponseWriter, r *http.Request) {
 		//리퀘스트 온거 파싱
 		imgFile, name, err := getData(r)
+		if err != nil {
+			fmt.Errorf("getFeature error, Error: ", err.Error())
+			return
+		}
 		//파싱한거 보내서 feature vector 얻어오기
 		var feature face.Descriptor
 		feature, err = getFeature(imgFile, URL)
@@ -54,6 +59,7 @@ func main() {
 			return
 		}
 		//DB에 저장 - 지금은 JSON파일로 저장
+		fmt.Println(name)
 		fmt.Println(feature)
 		//Signup(w, r)
 	}).Methods(http.MethodPost)
@@ -127,9 +133,12 @@ func getFeature(r io.Reader, u string) (face.Descriptor, error) {
 	if err != nil {
 		return f, err
 	}
+	defer resp.Body.Close()
 
+	if resp.Body == nil {
+		return f, errors.New("empty response body")
+	}
 	rs := new(Resp)
 	json.NewDecoder(resp.Body).Decode(rs)
-
 	return rs.Feature, nil
 }
