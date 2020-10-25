ede507196c59faf2a8142c629c41035c46189092
gurunelee <dlckdgk4858@gmail.com>
2020-10-25 18:02:11 +0900

    detach functions from main.go

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
 	"log"
-	"mime/multipart"
 	"net/http"
-	"strings"
 
 	face "github.com/Kagami/go-face"
 	"github.com/gorilla/mux"
@@ -32,6 +26,7 @@ type Meta struct {
 const (
 	SuccesMsg string = "signup success"
 	ErrorMsg  string = "you got some errors"
+	URL       string = "http://116.89.189.52:8080/get/feature"
 )
 
 /*
@@ -46,110 +41,8 @@ func main() {
 	fmt.Println("Facial-Auth-server started")
 	router := mux.NewRouter()
 
-	URL := "http://116.89.189.52:8080/get/feature"
-
 	// signup/face
-	router.HandleFunc("/signup/face", func(w http.ResponseWriter, r *http.Request) {
-		//리퀘스트 온거 파싱
-		imgFile, mdata, err := getData(r)
-		if err != nil {
-			fmt.Println("getData error, Error: ", err.Error())
-			return
-		}
-		//파싱한거 보내서 feature vector 얻어오기
-		var feature face.Descriptor
-		feature, err = getFeature(imgFile, URL)
-		if err != nil {
-			fmt.Println("getFeature error, Error: ", err.Error())
-			return
-		}
-
-		// mdata -> json go value
-		md := new(Meta)
-		rs := strings.NewReader(mdata)
-		dec := json.NewDecoder(rs)
-		err = dec.Decode(md)
-		if err != nil {
-			fmt.Println("error in parsing the meta data, Error: ", err.Error())
-			return
-		}
-		//DB에 저장 - 지금은 JSON파일로 저장
-		fmt.Println(mdata)
-		fmt.Println(md.Name)
-		fmt.Println(feature)
-		//Signup(w, r)
-	}).Methods(http.MethodPost)
+	router.HandleFunc("/signup/face", Signup).Methods(http.MethodPost)
 
 	log.Fatal(http.ListenAndServe(":8081", router))
-}
-
-func getData(r *http.Request) (multipart.File, string, error) {
-	r.ParseMultipartForm(32 << 20)
-	var f multipart.File //nil file
-
-	imgFile, _, err := r.FormFile("user-face")
-	if err != nil {
-		return f, "", fmt.Errorf("error in FormFile(\"user-face\"): " + err.Error())
-	}
-	defer imgFile.Close()
-
-	/*
-			imgByte, err := ioutil.ReadAll(file)
-			if err != nil {
-			return f, "", err
-		}
-	*/
-
-	//mdata is just 'user name' now
-	mdata := r.PostFormValue("meta-data")
-	if mdata == "" {
-		return f, "", fmt.Errorf("error in FormValue(\"meta-data\"): %s\n", fmt.Errorf("no such key"))
-	}
-
-	return imgFile, mdata, err
-}
-
-func getFeature(r io.Reader, u string) (face.Descriptor, error) {
-	// make multipart body
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	var f face.Descriptor //nil descriptor
-	// make part
-	part, err := writer.CreateFormFile("face-img", "face-img")
-	if err != nil {
-		return f, err
-	}
-	_, err = io.Copy(part, r)
-	if err != nil {
-		return f, err
-	}
-	err = writer.Close()
-	if err != nil {
-		return f, err
-	}
-	// req 생성
-	req, err := http.NewRequest("POST", u, body)
-	if err != nil {
-		return f, err
-	}
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-
-	// rep받아서 json으로 바꾸기
-	c := &http.Client{}
-	resp, err := c.Do(req)
-	if err != nil {
-		return f, err
-	}
-	defer resp.Body.Close()
-
-	if resp.Body == nil {
-		return f, errors.New("empty response body")
-	}
-	rs := new(Resp)
-	json.NewDecoder(resp.Body).Decode(rs)
-
-	if resp.StatusCode == http.StatusUnauthorized {
-		return f, fmt.Errorf("responsed error msg: " + rs.Error)
-	}
-	return rs.Feature, nil
 }
